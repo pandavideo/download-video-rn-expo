b96b455e2caced86e8a9982094878041f56c36d6
Amarildo Lima <amarildo.lima93@gmail.com>
2024-06-28 14:28:13 -0300

    init project

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
+import React, { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { Video } from 'expo-av';
+
+const PANDA_API_BASE_URL = `https://download-fr01.pandavideo.com:7443/videos`
+const PANDAS_API_KEY = 'panda-XXXXXXX'
+
+export async function downloadVideo(video_id, onProgress) {
+  try {
+    const downloadLink = `${PANDA_API_BASE_URL}/${video_id}/hd/download`;
+
+    const headers = { 'Authorization': PANDAS_API_KEY };
+
+    const path = FileSystem.documentDirectory + `video-${video_id}.mp4`;
+
+    const downloadResumable = FileSystem.createDownloadResumable(
+      downloadLink,
+      path,
+      { headers },
+      (data) => {
+        const progress = data.totalBytesWritten / data.totalBytesExpectedToWrite;
+        onProgress(progress);
+      }
+    );
+
+    const file = await downloadResumable.downloadAsync();
+    console.log(file)
+    return file.uri;
+  } catch (error) {
+    return null;
+  }
+}
 
 export default function App() {
+  const [progress, setProgress] = useState(0);
+  const [uri, setUri] = useState(null);
+  const videoRef = useRef(null);
+
+  const handleDownload = async () => {
+    const video_id = 'XXXXXX';
+    const uri = await downloadVideo(video_id, setProgress);
+    setUri(uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Download Video" onPress={handleDownload} />
+      <Text>Progress: {Math.round(progress * 100)}%</Text>
+      {uri && (
+        <>
+          <Text>Download Complete: {uri}</Text>
+          <Video
+            ref={videoRef}
+            source={{ uri }}
+            style={styles.video}
+            useNativeControls
+            resizeMode="contain"
+            isLooping
+            onPlaybackStatusUpdate={status => console.log(status)}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -16,5 +73,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  video: {
+    marginTop: 15,
+    width: '100%',
+    height: 300,
+    backgroundColor: 'black'
   },
 });
